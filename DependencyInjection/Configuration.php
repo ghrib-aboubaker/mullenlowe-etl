4e3a522509648fa9e73d002c83d46499ed45ee42
Aboubaker Ghrib <aboubaker.ghrib@mullenlowe.com>
2017-09-04 11:14:04 +0200

    change configuration class

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -20,9 +20,17 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('mullenlowe_etl');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
+        $rootNode
+            ->children()
+                ->scalarNode('param1')
+                    ->isRequired()
+                    ->cannotBeEmpty()
+                ->end() // end param1
+                ->scalarNode('param2')
+                    ->isRequired()
+                    ->cannotBeEmpty()
+                ->end() // end param2
+            ->end();
 
         return $treeBuilder;
     }
