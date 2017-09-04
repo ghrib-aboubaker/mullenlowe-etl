85569ccebe4d251d3df97594a21982617350ef2b
Aboubaker Ghrib <aboubaker.ghrib@mullenlowe.com>
2017-09-04 13:28:04 +0200

    correct parametering bundle

--- Service/ClassA.php
+++ Service/ClassA.php
@@ -4,8 +4,17 @@ namespace Mullenlowe\EtlBundle\Service;
 
 class ClassA
 {
+    private $param1;
+    private $param2;
+
+    public function __construct($param1, $param2)
+    {
+        $this->param1 = $param1;
+    	$this->param2 = $param2;
+    }
+
     public function getGreeting()
     {
-        return "Hello World !";
+        return $this->param1 . ' ' . $this->param2;
     }
 }
